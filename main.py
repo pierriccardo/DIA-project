851cc5ef087d65b434d4e936c7e8fcbf6bb2ea14
DavideZFC <davide.maran@mail.polimi.it>
2021-05-06 11:49:24 +0200

    aggregato (ma non funziona)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import yaml
+from pricing import *
 
 with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
@@ -6,3 +7,14 @@ with open('config.yml', 'r') as file:
 
 print(config["seed"])
 print(config["conv_rate"]['young']['interested'])
+
+
+bid=0.5
+x=5.0
+
+print(aggregated_new_cliks(bid))
+
+# print(aggregated_conv_rate(x))
+
+print(aggregated_return_proba(bid))
+ 
