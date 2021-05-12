b4f7d3861daa6cf1989868cd4b51872b70e53789
pierriccardo <pierriccardo.olivieri@mail.polimi.it>
2021-05-12 22:38:06 +0200

    fixes

--- plotter.py
+++ plotter.py
@@ -162,7 +162,7 @@ class Plotter:
             ax[index].set_xlabel("Number of comebacks")
 
             ax[index].hist(
-                return_probability(_lambda, size = 10000),
+                return_probability(_lambda),
                 14,
                 density=True,
                 color=color,
